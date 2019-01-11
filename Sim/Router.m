8357a13780e74dc32e40b142efad1c1a8c0669b4
Yantong WANG <yantong.wang@kcl.ac.uk>
2019-01-11 18:22:36 +0000

    establish object for router

---
+++ Sim/Router.m
@@ -0,0 +1,23 @@
+classdef Router
+    % Original network router in ICN-based RAN
+    
+    properties
+        posX;
+        posY;
+    end
+    
+    methods
+        function obj = Router(inputArg1,inputArg2)
+            %ROUTER Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
